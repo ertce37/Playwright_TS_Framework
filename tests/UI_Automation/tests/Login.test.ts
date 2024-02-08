868db929bdd2dfb788411437038b13290f56f55a
RulYuce <ertugrulyuce@gmail.com>
2024-02-07 22:22:07 -0600

    added barmenu test

--- tests/UI_Automation/tests/Login.test.ts
+++ tests/UI_Automation/tests/Login.test.ts
@@ -8,8 +8,20 @@ test.beforeEach(async ({page}) => {
     login = new LoginPage(page);
     await login.goto();
 });
-test('sign in with success',async({page}) =>{
-    await login.signIn(process.env.USERNAME as string, process.env.PASSWORD as string)
-    expect(await page.title()).not.toBeNull()
+// test('sign in with success',async({page}) =>{
+//     await login.signIn(process.env.USERNAME as string, process.env.PASSWORD as string)
+//     expect(await page.title()).not.toBeNull()
 
+// })
+
+test("verify left bar tab names",async({page})=>{
+    const barMenuNames = []
+    await login.signIn("cgrbtl8991@gmail.com","Ertce82@")
+    await page.waitForTimeout(3000)
+
+    const barMenus = page.locator('#fuse-vertical-navigation-item-wrapper')
+
+    await page.waitForTimeout(3000)
+   
+    expect(await barMenus.allInnerTexts()).toEqual(barMenuNames)
 })
